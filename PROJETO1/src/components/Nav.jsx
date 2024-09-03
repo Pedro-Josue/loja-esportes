45525dcc12b8df2e4295e80bdcc03501cbc8f20d
Pedro Josue <pedro.josue.ap@outlook.com>
2024-09-03 15:43:33 -0300

    Chamando o styled-components, e com ele começando a fazer a Navbar

--- PROJETO1/src/components/Nav.jsx
+++ PROJETO1/src/components/Nav.jsx
@@ -1,13 +1,16 @@
 import {Link} from 'react-router-dom'
+import { NavStyle } from '../css/NavStyle'
 // importando o link do react-router-dom, para conseguir criar as rotas na navbar
 const Nav=()=>{
     return(
-        <>
-        <>Home</>
-        <>Produtos</>
-        <>Contato</>
-        <>Sobre</>
-        </>
+        <NavStyle>
+            <section className='nav'>
+                <Link to='/' className='navlink'>Home</Link>
+                <Link to='/produtos' className='navlink'>Produtos</Link>
+                <Link to='/contato' className='navlink'>Contato</Link>
+                <Link to='sobre' className='navlink'>Sobre</Link>
+            </section>
+        </NavStyle>
     )
 }
 
---
+++ PROJETO1/src/css/NavStyle.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const NavStyle =styled.section `
+
+.nav{
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    gap: 1rem;
+    background-color: #c7c7c7;
+}
+.navlink{
+    font-size:1rem;
+    color: black;
+    text-decoration: none;
+    padding: 20px;
+}
+
+`
