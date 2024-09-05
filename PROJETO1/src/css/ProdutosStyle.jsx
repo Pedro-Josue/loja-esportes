7acde53001527b9a640e6383a8703e95908002ee
DevRuanVieira <ruanmelovieira01@gmail.com>
2024-09-05 09:06:42 -0300

    Criando os cards

--- PROJETO1/src/css/ProdutosStyle.jsx
+++ PROJETO1/src/css/ProdutosStyle.jsx
@@ -1,7 +1,56 @@
 import styled from "styled-components";
 
 export const ProdutosStyle = styled.section `
-    
+
+
+#h1_categorias{
+    padding: 2%;
+    text-align: center;
+
+
+
+}
+.categorias{
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+}
+
+.card{
+    display: flex;
+    justify-content: center;
+    border-radius: 5px;
+    transition: 0.3s;
+    cursor: pointer;
+    background-color: #f3f1f1;
+    padding: 5%;
+    flex-direction: column;
+    gap: 4rem;
+  
+    border-radius: 10px;
+}
+
+.card_img{
+    width: 200px;
+    margin-top: 5px;
+    margin-bottom: 5px;
+    display: flex;
+    align-items: center;
+
+
+}
+
+.card h4{
+    margin-top: 20%;
+    color: #2e2e2e;
+    font-size: 1.5em;
+    position: relative;
+    margin-right: 15%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+}
 
     
 `
--- PROJETO1/src/routes/Produtos.jsx
+++ PROJETO1/src/routes/Produtos.jsx
@@ -1,10 +1,36 @@
 import { ProdutosStyle } from "../css/ProdutosStyle"
+import corda from "../assets/corda.png"
+import halter from "../assets/halter.png"
+import tenis from "../assets/tenis.png"
+import tabela from "../assets/tabela.png"
 
 const Produtos=()=>{
     return(
-        <>
-        <p>halter</p>
-        </>
+        <ProdutosStyle>
+        
+        
+            <h1 id="h1_categorias"> Navegue por categorias</h1>
+            <div className="categorias">
+                <div className="card">
+                    <img src={halter} className="card_img" />
+                    <h4>Halteres 10KG</h4>
+                </div>
+                <div className="card">
+                    <img src={corda} className="card_img"  />
+                    <h4>Corda</h4>
+                </div>
+                <div className="card">
+                    <img src={tenis} className="card_img"  />
+                    <h4>Tenis</h4>
+                </div>
+                <div className="card">
+                    <img src={tabela} className="card_img" />
+                    <h4>Acess</h4>
+                </div>
+            </div>
+       
+
+        </ProdutosStyle>
     )
 }
 
